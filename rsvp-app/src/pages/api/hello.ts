3d594758a957aee04b7399e35c39ec019dd2db22
Jack Rose <user@jackrose.co.nz>
2023-03-04 20:45:09 +1100

    pull data from google sheets (working!)

--- rsvp-app/src/pages/api/hello.ts
+++ rsvp-app/src/pages/api/hello.ts
@@ -1,13 +1,36 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import * as google from '@googleapis/sheets'
 
 type Data = {
   name: string
+  data?: null | any[][]
 }
 
-export default function handler(
-  req: NextApiRequest,
+export default async function handler(
+  _req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+
+  const auth = new google.auth.GoogleAuth({
+    keyFilename: "./key.json",
+    scopes: [
+      'https://www.googleapis.com/auth/drive',
+      'https://www.googleapis.com/auth/drive.file',
+      'https://www.googleapis.com/auth/spreadsheets',
+    ],
+  });
+  const authClient = await auth.getClient();
+
+  const s = google.sheets({
+    version: 'v4',
+    auth: authClient
+  })
+
+  const x = await s.spreadsheets.values.get({
+    spreadsheetId: "1QJCNxD9dDkBoDmK0hccRRrfyaUWrpj0OIUQUESxlzrM",
+    range: 'rsvp!A:B'
+  })
+
+  res.status(200).json({ name: 'x', data: x.data.values })
 }
