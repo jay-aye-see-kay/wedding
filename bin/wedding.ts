10c6a6692ae06accc720ed61108d0f7bee95c233
Jack Rose <user@jackrose.co.nz>
2023-03-04 15:21:22 +1100

    get a sample function and api gateway working

--- bin/wedding.ts
+++ bin/wedding.ts
@@ -15,7 +15,7 @@ new WeddingStack(app, 'WeddingStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+  // env: { account: '622733396373', region: 'ap-southeast-2' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
--- lib/wedding-stack.ts
+++ lib/wedding-stack.ts
@@ -1,16 +1,24 @@
 import * as cdk from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import path from 'path';
 
 export class WeddingStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const mainFunction = new NodejsFunction(this, 'MainFunction', {
+      entry: path.join(__dirname, "lambdas/main.ts"),
+      runtime: lambda.Runtime.NODEJS_18_X
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'WeddingQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const gateway = new apigw.LambdaRestApi(this, 'MainEndpoint', {
+      handler: mainFunction
+    });
+
+    new cdk.CfnOutput(this, 'MainFunctionName', { value: mainFunction.functionName, })
+    new cdk.CfnOutput(this, 'ApiGatewayUrl', { value: gateway.url })
   }
 }
