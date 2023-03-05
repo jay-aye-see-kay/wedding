b788c8b5abeca4a469177cc4f4926ee39368abae
Jack Rose <user@jackrose.co.nz>
2023-03-05 22:23:43 +1100

    add navbar

--- rsvp-app/src/components/page-wrapper.tsx
+++ rsvp-app/src/components/page-wrapper.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import eucalyptusImg from "../../wedding-images/eucalyptus-2.png"
 
 const defaultPageTitle = "Nora & Jack's Wedding"
@@ -18,12 +19,28 @@ export function PageWrapper(props: Props) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" /> {/* TODO: add favicon*/}
       </Head>
+
+      <div className="navbar bg-base-100">
+        <div className="flex-1"></div>
+        <div className="flex-none">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <Link href="/">About</Link>
+            </li>
+            <li>
+              <Link href="/rsvp">RSVP</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+
       <Image
         alt=""
         src={eucalyptusImg}
         className="absolute"
         style={{ transform: "translateY(-35%) translateX(-12%) rotate(170deg)" }}
       />
+
       <>{props.children}</>
     </>
   )
