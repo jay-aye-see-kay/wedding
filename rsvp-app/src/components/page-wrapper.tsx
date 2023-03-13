537c6be39dd303fc40694745ae872ad11dc5cf2c
Jack Rose <user@jackrose.co.nz>
2023-03-13 14:56:22 +1100

    stop iOS auto zooming in on form elements

--- rsvp-app/src/components/page-wrapper.tsx
+++ rsvp-app/src/components/page-wrapper.tsx
@@ -15,7 +15,7 @@ export function PageWrapper(props: Props) {
       <Head>
         <title>{props.pageTitle ?? defaultPageTitle}</title>
         <meta name="description" content={defaultPageTitle} />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
