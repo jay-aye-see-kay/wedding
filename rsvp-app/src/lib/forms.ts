e3bcd935b26370db5662c31588c98af84cb6ed9d
Jack Rose <user@jackrose.co.nz>
2023-03-13 14:56:02 +1100

    form more accepting of inputs

--- rsvp-app/src/lib/forms.ts
+++ rsvp-app/src/lib/forms.ts
@@ -2,10 +2,10 @@ import z from 'zod'
 
 /** this is a schema for forms, so all values string and empty string means undefined */
 export const rsvpSchema = z.object({
-  names: z.string().min(1, "Name(s) are required"),
+  names: z.string().trim().min(1, "Name(s) are required"),
   dietaries: z.string(),
   notes: z.string(),
-  secret: z.string().min(1, "This secret code is required, check the bottom of your invite"),
+  secret: z.string().trim().toLowerCase().min(1, "This secret code is required, check the bottom of your invite"),
 });
 
 export type RsvpSchema = z.infer<typeof rsvpSchema>
